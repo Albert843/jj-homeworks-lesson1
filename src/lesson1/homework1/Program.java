9e6d002d7bc74a15517600af3475e3e00592fc13
Albert843 <nuralbert@yandex.ru>
2024-02-02 20:41:54 +0300

    changed homework/Program.java

--- src/lesson1/homework1/Program.java
+++ src/lesson1/homework1/Program.java
@@ -7,7 +7,10 @@ public class Program {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         double average = numbers.stream()
-                .filter(num -> num % 2 == 0).mapToInt((num) -> num).summaryStatistics().getAverage();
+                .filter(num -> num % 2 == 0)
+                .mapToInt((num) -> num)
+                .summaryStatistics()
+                .getAverage();
         System.out.printf("Среднее значение всех четных чисел: " + average);
     }
 }
